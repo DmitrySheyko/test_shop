e7780d3cb21ee37c04f944b3910bff7803c2b869
DmitrySheyko <Student-ne@yandex.ru>
2023-03-23 16:54:48 +0300

    Feat: replace admin insert

--- src/main/java/com/example/test_shop/security/AddAdmin.java
+++ src/main/java/com/example/test_shop/security/AddAdmin.java
@@ -1,28 +1,14 @@
 package com.example.test_shop.security;
 
-import com.example.test_shop.user.dto.NewUserDto;
-import com.example.test_shop.user.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class AddAdmin implements CommandLineRunner {
 
-    private final UserService service;
 
     @Override
     public void run(String... args) throws Exception {
-
-        NewUserDto user = NewUserDto.builder()
-                .username("admin")
-                .password("admin")
-                .role("ROLE_ADMIN")
-                .email("admin@email.com")
-                .build();
-
-        service.add(user);
         System.out.println("Admin username: admin \nAdmin password: admin");
 
     }
