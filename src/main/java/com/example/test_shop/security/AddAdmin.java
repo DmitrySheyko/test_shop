ddb67623df712232887f800961f53b23f8f75b78
DmitrySheyko <Student-ne@yandex.ru>
2023-03-23 18:11:39 +0300

    Fix: fix mistakes

--- src/main/java/com/example/test_shop/security/AddAdmin.java
+++ src/main/java/com/example/test_shop/security/AddAdmin.java
@@ -1,16 +1,31 @@
 package com.example.test_shop.security;
 
+import com.example.test_shop.user.dto.NewUserDto;
+import com.example.test_shop.user.repository.UserRepository;
+import com.example.test_shop.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AddAdmin implements CommandLineRunner {
 
+    private final UserService service;
+    private final UserRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
+        if (!repository.existsByUsername("admin")) {
+            NewUserDto admin = NewUserDto.builder()
+                    .username("admin")
+                    .password("admin")
+                    .email("admin@email.ru")
+                    .role("ROLE_ADMIN")
+                    .build();
+            service.add(admin);
+        }
         System.out.println("Admin username: admin \nAdmin password: admin");
-
     }
 
 }
